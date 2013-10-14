d419d78322564767d6f292d87a1a2dea4ff951d2
Martin <fous.martin@gmail.com>
2013-10-14 20:30:00 +0200

    vyplneny gettery a settery pro tridu edge

--- guide-me/src/cz/fjfi/guideme/core/Edge.java
+++ guide-me/src/cz/fjfi/guideme/core/Edge.java
@@ -1,7 +1,9 @@
 package cz.fjfi.guideme.core;
 
+import java.util.List;
+
 /**
- * Thi class represents simple edge from one node to another in GuideMe map. It
+ * This class represents simple edge from one node to another in GuideMe map. It
  * provides getters and setters for it attributes.
  * 
  * @author
@@ -13,7 +15,15 @@ public class Edge
 //== CLASS CONSTANTS ===========================================================
 //== CLASS VARIABLES ===========================================================
 //== INSTANCE VARIABLES ========================================================
-
+	private String guid;
+	private String name;
+	private long timeDistance;
+	private Node start;
+	private Node end;
+	private Direction direction;
+	private String description;
+	private Map myMap;
+	private List<Location> locations;
 //==============================================================================
 //== CONSTRUCTORS ==============================================================
     
@@ -21,7 +31,19 @@ public class Edge
      * TODO: comment
      */
     //TODO: constructor parameters and body
-    public Edge(){}
+    public Edge(String guid, String name, long timeDistance, Node start, Node end, Direction direction,
+    			String description, Map myMap, List<Location> locations)
+    {
+    	this.guid = guid;
+    	this.name = name;
+    	this.timeDistance = timeDistance;
+    	this.start = start;
+    	this.end = end;
+    	this.direction = direction;
+    	this.description = description;
+    	this.myMap = myMap;
+    	this.locations = locations;
+    }
     
 //== GETTERS AND SETTERS =======================================================
     
@@ -29,91 +51,91 @@ public class Edge
      * TODO: comment
      */
     //TODO: body
-    public final String getGUID(){return "";}
+    public final String getGUID(){return this.guid;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final String getName(){return "";}
+    public final String getName(){return this.name;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final long getTimeDistance(){return 0L;}
+    public final long getTimeDistance(){return this.timeDistance;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final Node getStart(){return null;}
+    public final Node getStart(){return this.start;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final Node getEnd(){return null;}
+    public final Node getEnd(){return this.end;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final Direction getDirection(){return null;}
+    public final Direction getDirection(){return this.direction;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final String getDescription(){return "";}
+    public final String getDescription(){return this.description;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    protected final Map getMyMap(){return null;}
+    protected final Map getMyMap(){return this.myMap;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setGUID(String newGUID){}
+    public final void setGUID(String newGUID){this.guid = newGUID;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setName(String newName){}
+    public final void setName(String newName){this.name = newName;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setTimeDistance(long newTimeDistance){}
+    public final void setTimeDistance(long newTimeDistance){this.timeDistance = newTimeDistance;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setStart(Node newStart){}
+    public final void setStart(Node newStart){this.start = newStart;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setEnd(Node newEnd){}
+    public final void setEnd(Node newEnd){this.end = newEnd;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setDirection(Direction newDirection){}
+    public final void setDirection(Direction newDirection){this.direction = newDirection;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setDescription(String newDescription){}
+    public final void setDescription(String newDescription){this.description = newDescription;}
     
 //== OTHER METHODS =============================================================
 }
