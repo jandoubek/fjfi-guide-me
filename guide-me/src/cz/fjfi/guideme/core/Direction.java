5d091ecdd0a0ddda91f098def85c13283352770f
Martin <fous.martin@gmail.com>
2013-10-14 20:50:58 +0200

    fce pro zatoceni vlevo a vpravo

--- guide-me/src/cz/fjfi/guideme/core/Direction.java
+++ guide-me/src/cz/fjfi/guideme/core/Direction.java
@@ -3,6 +3,7 @@ package cz.fjfi.guideme.core;
 /**
  * Thic enumeration contains all allowed directions in GuideMe app and methods
  * to work with them.
+ * Feel free to modify :-D
  * @author
  * @version
  */
@@ -16,4 +17,21 @@ public enum Direction
     Southwest,
     West,
     Northwest;
+    
+    public Direction turnRight()
+    {
+    	Direction[] directions = Direction.values();
+    	int ordinal = this.ordinal();
+    	ordinal = ++ordinal % directions.length;
+    	return directions[ordinal];
+    }
+    
+    public Direction turnLeft()
+    {
+    	Direction[] directions = Direction.values();
+    	int ordinal = this.ordinal();
+    	ordinal = --ordinal % directions.length;
+    	return directions[ordinal];
+    }
+
 }
