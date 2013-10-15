4bd430ab07878203d7207be74e10e1d5d8f27222
matejantos <matey.antos@gmail.com>
2013-10-15 21:47:50 +0200

    Added Class Resource Manager

---
+++ guide-me/src/cz/fjfi/guideme/android/ResourceManager.java
@@ -0,0 +1,18 @@
+package cz.fjfi.guideme.android;
+
+import cz.fjfi.guideme.core.*;
+
+/******************************************************************************
+ * This class works with file resources of GuideMe app. Its main goal is to 
+ * load and save XML files containing GuideMe map.
+ * @author
+ * @version
+ */
+public class ResourceManager
+{
+
+	public static Map loadMap(String fileName)
+	{
+		return null;
+	}
+}
