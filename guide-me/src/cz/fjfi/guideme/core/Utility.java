bedeb5824cfbd1e7a3e78ce4ca1169ded71eb611
matejantos <matey.antos@gmail.com>
2013-10-15 21:52:20 +0200

    Added Utility class

---
+++ guide-me/src/cz/fjfi/guideme/core/Utility.java
@@ -0,0 +1,11 @@
+package cz.fjfi.guideme.core;
+
+/******************************************************************************
+ * This class provides utility methods for GuideMe app and its core objects.
+ * @author
+ * @version
+ */
+public class Utility
+{
+    public static String generateGUID(){return "";}
+}
