c298cf30d78540db8a8ea734e8445f09d445b39b
benetkat <benetkat@fjfi.cvut.cz>
2013-10-16 22:49:30 +0200

    gettery a settery v Location

--- guide-me/src/cz/fjfi/guideme/core/Location.java
+++ guide-me/src/cz/fjfi/guideme/core/Location.java
@@ -11,6 +11,10 @@ public class Location
 //== CLASS CONSTANTS ===========================================================
 //== CLASS VARIABLES ===========================================================
 //== INSTANCE VARIABLES ========================================================
+	private UUID guid;
+	private String name;
+	private String description;
+	private Map myMap;
 
 //==============================================================================
 //== CONSTRUCTORS ==============================================================
@@ -19,7 +23,13 @@ public class Location
      * TODO: comment
      */
     //TODO: constructor parameters and body
-    public Location(){}
+    public Location(UUID guid, String name, String description, Map myMap)
+	{
+		this.guid = guid;
+		this.name = name;
+		this.description = description;
+		this.myMap = myMap;
+	}
     
 //== GETTERS AND SETTERS =======================================================
     
@@ -27,43 +37,43 @@ public class Location
      * TODO: comment
      */
     //TODO: body
-    public final UUID getGUID(){return null;}
+    public final UUID getGUID(){return this.guid;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final String getName(){return "";}
+    public final String getName(){return this.name;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final String getDescription(){return "";}
+    public final String getDescription(){return this.description;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    protected final Map getMyMap(){return null;}
+    protected final Map getMyMap(){return this.myMap;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setGUID(UUID newGUID){}
+    public final void setGUID(UUID newGUID){this.guid = newGUID;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setName(String newName){}
+    public final void setName(String newName){this.name = newName;}
     
     /***************************************************************************
      * TODO: comment
      */
     //TODO: body
-    public final void setDescription(String newDescription){}
+    public final void setDescription(String newDescription){this.description = newDescription;}
     
 //== OTHER METHODS =============================================================
    
