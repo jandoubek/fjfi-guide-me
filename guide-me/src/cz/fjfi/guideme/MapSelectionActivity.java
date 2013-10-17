076a2459fe85203a476edf9a0c18c963b5977e2e
V.K. <vladislav.klima@gmail.com>
2013-10-17 12:48:35 +0200

    map selection activity

---
+++ guide-me/src/cz/fjfi/guideme/MapSelectionActivity.java
@@ -0,0 +1,15 @@
+package cz.fjfi.guideme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapSelectionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+
+}
