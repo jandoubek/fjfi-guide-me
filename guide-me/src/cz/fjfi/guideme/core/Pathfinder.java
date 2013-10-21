f5b291aa8d3f4b65ea49b1f63f509ae3b3b882e8
TheMartin <64.Martin@gmail.com>
2013-10-21 19:02:43 +0200

    Pathfinder refactoring

--- guide-me/src/cz/fjfi/guideme/core/Pathfinder.java
+++ guide-me/src/cz/fjfi/guideme/core/Pathfinder.java
@@ -9,67 +9,106 @@ import java.util.*;
  */
 public class Pathfinder {
     private Map map;
+    private java.util.Map<Node, Long> distances;
+    private java.util.Map<Node, Node> previous;
+    private Set<Node> openSet;
     
     public Pathfinder(Map map)
     {
         this.map = map;
+        this.distances = new HashMap<Node, Long>();
+        this.previous = new HashMap<Node, Node>();
+        this.openSet = new HashSet<Node>(map.getNodes()); 
     }
     
+    /**
+     * finds the shortest route between nodes 'from' and 'to'
+     */
     public final List<Node> findRouteBetween(Node from, Node to)
     {
-        // initialize
-        List<Node> route = new ArrayList<Node>();
-        
-        // distances set to infinity, except for start, which is set to 0
-        java.util.Map<Node, Long> distances = new HashMap<Node, Long>();
-        for (Node n : map.getNodes()) {
-            distances.put(n, Long.MAX_VALUE);
-        }
-        distances.put(from, Long.valueOf(0));
-        
-        // previous node on the shortest discovered path
-        java.util.Map<Node, Node> previous = new HashMap<Node, Node>();
-        
-        // nodes not yet visited
-        Set<Node> openSet = new HashSet<Node>(map.getNodes());
+        initializeDistances(from);
         
         while (!openSet.isEmpty()) {
-            // find closest node not yet visited
-            long minDistance = Long.MAX_VALUE;
-            Node closestOpenNode = from;
-            for (Node n : openSet) {
-                long currentDistance = distances.get(n);
-                if (minDistance < currentDistance) {
-                    minDistance = currentDistance;
-                    closestOpenNode = n;
-                }
-            }
+            Node closestOpenNode = getClosestOpenNode();
+            openSet.remove(closestOpenNode);
+            
             if (closestOpenNode == to) {
                 break; // reached target, end search
             }
-            openSet.remove(closestOpenNode);
 
-            long distanceToClosestNode = minDistance;
+            long distanceToClosestNode = distances.get(closestOpenNode);
             if (distanceToClosestNode == Long.MAX_VALUE) {
-                return route; // no route exists, returns empty list
-                // TODO: throw a proper exception
+                throw new IllegalArgumentException("No route exists.");
             }
             
-            // update distances and previous for all neighbors
-            List<Node> neighbors = map.getNeighborsOf(closestOpenNode);
-            for (Node n : neighbors) {
-                if (openSet.contains(n)) {
-                    Edge connectingEdge = map.getEdgeConnectingNodes(closestOpenNode, n);
-                    long newDistance = distanceToClosestNode + connectingEdge.getTimeDistance();
-                    if (newDistance < distances.get(n)) {
-                        distances.put(n, newDistance);
-                        previous.put(n, closestOpenNode);
-                    }
-                }
+            updatePaths(closestOpenNode);
+        }
+
+        return constructRoute(from, to);
+    }
+
+    /**
+     * initialize the distances array
+     */
+    private void initializeDistances(Node from)
+    {
+        for (Node n : map.getNodes()) {
+            distances.put(n, Long.MAX_VALUE);
+        }
+        distances.put(from, Long.valueOf(0));      
+    }
+
+    /**
+     * find the node in openSet with lowest distance
+     */
+    private Node getClosestOpenNode()
+    {
+        long minDistance = Long.MAX_VALUE;
+        Node closestOpenNode = openSet.iterator().next();
+        for (Node n : openSet) {
+            long currentDistance = distances.get(n);
+            if (minDistance < currentDistance) {
+                minDistance = currentDistance;
+                closestOpenNode = n;
             }
         }
-        
-        // construct the route
+        return closestOpenNode;
+    }
+
+    /**
+     * update the distances and previous arrays if necessary
+     */
+    private void updatePaths(Node closestOpenNode)
+    {
+        long distanceToClosestNode = distances.get(closestOpenNode);
+        List<Node> neighbors = map.getNeighborsOf(closestOpenNode);
+        for (Node n : neighbors) {
+            if (openSet.contains(n)) {
+                updatePathFor(n, closestOpenNode, distanceToClosestNode);
+            }
+        }        
+    }
+
+    /**
+     * update the distance and previous arrays for node 'n' if necessary
+     */
+    private void updatePathFor(Node n, Node closestOpenNode,
+            long distanceToClosestNode)
+    {
+        Edge connectingEdge = map.getEdgeConnectingNodes(closestOpenNode, n);
+        long newDistance = distanceToClosestNode + connectingEdge.getTimeDistance();
+        if (newDistance < distances.get(n)) {
+            distances.put(n, newDistance);
+            previous.put(n, closestOpenNode);
+        }
+    }
+
+    /**
+     * construct the route from the distance and previous arrays
+     */
+    private List<Node> constructRoute(Node from, Node to)
+    {
+        List<Node> route = new ArrayList<Node>();
         Node currentNode = to;
         route.add(currentNode);
         while (currentNode != from) {
@@ -77,7 +116,6 @@ public class Pathfinder {
             route.add(currentNode);
         }
         Collections.reverse(route);
-
         return route;
     }
 }
