2bb606788e5003a7ef070abc39bdf11f1fd010ce
Tomáš Juřík <magog2@seznam.cz>
2013-10-24 13:50:02 +0200

    Vytvoření TextView a tlačítka Start v protoypu Activity

--- guide-me/src/cz/fjfi/guideme/NavigateActivity.java
+++ guide-me/src/cz/fjfi/guideme/NavigateActivity.java
@@ -3,13 +3,18 @@ package cz.fjfi.guideme;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class NavigateActivity extends Activity {
+	public TextView vypis;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_navigate);
+		vypis = (TextView) findViewById(R.id.navigate_tv_vypis);
 	}
 
 	@Override
@@ -17,6 +22,12 @@ public class NavigateActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.navigate, menu);
 		return true;
+	}
+	
+	public void navigate_bt_start_OnClick(View view)
+	{
+		NavigateAsync navigateAsync = new NavigateAsync(this);
+		navigateAsync.execute("");
 	}
 
 }
