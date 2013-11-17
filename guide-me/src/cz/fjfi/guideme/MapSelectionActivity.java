04c7f149c7c9f2f72da202a5a00dd09f88758a79
V.K. <vladislav.klima@gmail.com>
2013-11-17 11:27:40 +0100

    dialog pro stazeni mapy

--- guide-me/src/cz/fjfi/guideme/MapSelectionActivity.java
+++ guide-me/src/cz/fjfi/guideme/MapSelectionActivity.java
@@ -1,43 +1,56 @@
 package cz.fjfi.guideme;
 
 import java.util.ArrayList;
-
-import cz.fjfi.guideme.android.MapsBaseAdapter;
-import cz.fjfi.guideme.core.GMMapHeader;
+import java.util.Collections;
+import java.util.Comparator;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import cz.fjfi.guideme.android.MapsBaseAdapter;
+import cz.fjfi.guideme.android.MapsDownloadBaseAdapter;
+import cz.fjfi.guideme.core.GMMapHeader;
 
 public class MapSelectionActivity extends Activity {
 
 	private ListView mapList;
 	private MapsBaseAdapter adapter;
 	private int clickedItem=-1;
+	private MapsDownloadBaseAdapter adpaterDownload;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map_selecion);
 
-		ArrayList<GMMapHeader> headers = new ArrayList<GMMapHeader>();
+		initWidgets();
+		initData();
 
-		for(int i = 1 ; i < 20 ; i++){
-			GMMapHeader header = new GMMapHeader();
-			header.setName("Mapa "+i);
-			header.setDescription("Popis mapy " +i);
-			headers.add(header);
-		}
 
+
+	}
+	private void initWidgets() {
+		Button downloadBT = (Button) findViewById(R.id.select_map_bt_download);
+		downloadBT.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				setDialog();
+			}
+		});
+		
 		mapList = (ListView)findViewById(R.id.select_map_lv_mapList);
-		adapter = new MapsBaseAdapter(this, headers);
-		mapList.setAdapter(adapter);
 		mapList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -54,7 +67,6 @@ public class MapSelectionActivity extends Activity {
 						button1.setVisibility(View.GONE);
 						button2 = (Button) oldView.findViewById(R.id.button2);
 						button2.setVisibility(View.GONE);
-						Log.i("MAP SELECTION", "hiding item:" + clickedItem);
 
 					}
 					description = (TextView) view.findViewById(R.id.item_description);
@@ -64,7 +76,6 @@ public class MapSelectionActivity extends Activity {
 					button2 = (Button) view.findViewById(R.id.button2);
 					button2.setVisibility(View.VISIBLE);
 					adapter.setClickedItem(position);
-					Log.i("MAP SELECTION", "showing item:" + clickedItem);
 				}else{
 					View oldView = adapterView.getChildAt(adapter.getClickedItem());
 					TextView description;
@@ -81,9 +92,98 @@ public class MapSelectionActivity extends Activity {
 					adapter.setClickedItem(-1);
 				}
 				adapter.notifyDataSetChanged();
-			}	
+			}
 		});
-
 	}
 
+	private void initData() {
+		ArrayList<GMMapHeader> headers = new ArrayList<GMMapHeader>();
+
+		for(int i = 1 ; i < 20 ; i++){
+			GMMapHeader header = new GMMapHeader();
+			header.setName("Mapa "+i);
+			header.setDescription("Popis mapy " +i);
+			headers.add(header);
+		}
+
+
+		adapter = new MapsBaseAdapter(this, headers);
+		mapList.setAdapter(adapter);
+	}
+
+
+
+	private void setDialog(){
+		final Dialog dialog1 = new Dialog(MapSelectionActivity.this);
+		dialog1.setContentView(R.layout.dialog_download_map);
+		dialog1.setTitle(getString(R.string.map_selection_d_title));
+		dialog1.setCancelable(true);
+
+
+		WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+		lp.copyFrom(dialog1.getWindow().getAttributes());
+		lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+		lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		ListView maps = (ListView) dialog1.findViewById(R.id.dialog_download_lv_maps);
+		EditText searchET = (EditText) dialog1.findViewById(R.id.dialog_download_et_search);
+		ArrayList<GMMapHeader> headers = new ArrayList<GMMapHeader>();
+
+		adpaterDownload = new MapsDownloadBaseAdapter(MapSelectionActivity.this, headers);
+		maps.setAdapter(adpaterDownload);
+
+		searchET.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if (s.equals("")) {
+					adpaterDownload.setOriginalHeaders();
+					adpaterDownload.notifyDataSetChanged();
+				} else {   	
+					adpaterDownload.setHeaders(searchSeznam(adpaterDownload.getHeadersOrig(), s.toString()));
+					adpaterDownload.notifyDataSetChanged();
+				}
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+
+			}
+		});
+
+		dialog1.show();
+		dialog1.getWindow().setAttributes(lp);
+	}
+
+	public ArrayList<GMMapHeader> searchSeznam(ArrayList<GMMapHeader> seznam, String compare){
+		ArrayList<GMMapHeader> seznamNal = new ArrayList<GMMapHeader>();
+
+		for(GMMapHeader polozka : seznam){
+			if(org.apache.commons.lang3.StringUtils.containsIgnoreCase(removeDiacritics(polozka.getName()), removeDiacritics(compare))){
+				seznamNal.add(polozka);				
+			}
+		}
+		sortList(seznamNal);
+		return seznamNal;
+	}
+	public static String removeDiacritics(String text) {
+		return  org.apache.commons.lang3.StringUtils.stripAccents(text);
+	}
+
+	private static void sortList(ArrayList<GMMapHeader> aItems){
+		Collections.sort(aItems, new NameComparator());
+	}
+
+	static class NameComparator implements Comparator<Object> {
+		public int compare(Object obj1, Object obj2)
+		{
+			GMMapHeader o1 = (GMMapHeader) obj1;
+			GMMapHeader o2 = (GMMapHeader) obj2;
+			return o1.getName().compareToIgnoreCase(o2.getName());
+		}
+	}
 }
---
+++ guide-me/src/cz/fjfi/guideme/android/MapsDownloadBaseAdapter.java
@@ -0,0 +1,109 @@
+package cz.fjfi.guideme.android;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import cz.fjfi.guideme.R;
+import cz.fjfi.guideme.core.GMMapHeader;
+
+public class MapsDownloadBaseAdapter extends BaseAdapter {
+	private ArrayList<GMMapHeader> headers;
+	private ArrayList<GMMapHeader> headersOrig;
+	private LayoutInflater mInflater;
+
+	public MapsDownloadBaseAdapter(Context context, ArrayList<GMMapHeader> headers) {
+		//sortList(headers);
+		this.headers = headers;
+		headersOrig = new ArrayList<GMMapHeader>(headers);
+		mInflater = LayoutInflater.from(context);
+	}
+
+	/**
+	 * Number of item in arraylist
+	 */
+	public int getCount() { 
+		return headers.size();
+	}
+
+	/**
+	 * Return header by given position
+	 * @param position position of item in arraylist
+	 */
+	public GMMapHeader getItem(int position) {
+		return headers.get(position);
+	}
+
+	/**
+	 * Add new header to arraylist
+	 * @param header header to add
+	 */
+	public void add(GMMapHeader header){
+		headers.add(header);
+		headersOrig.add(header);
+		sortList(headers);
+		sortList(headersOrig);
+	}
+	
+	public void setOriginalHeaders(){
+		sortList(headersOrig);
+		headers = new ArrayList<GMMapHeader>(headersOrig);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public ArrayList<GMMapHeader> getHeaders(){
+		return headers;
+	}
+	public ArrayList<GMMapHeader> getHeadersOrig(){
+		return headers;
+	}
+
+	public void setHeaders(ArrayList<GMMapHeader> headers){
+		this.headers = headers;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) { 
+			convertView = mInflater.inflate(R.layout.item_download_map, null);
+			holder = new ViewHolder();
+			holder.itemDescription = (TextView) convertView.findViewById(R.id.item_download_tv_description);
+			holder.itemName = (TextView) convertView.findViewById(R.id.item_download_tv_name);
+
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		holder.itemDescription.setText(headers.get(position).getDescription()); 
+		holder.itemName.setText(headers.get(position).getName());  
+		return convertView;
+	}
+
+	static class ViewHolder {
+		TextView itemName;
+		TextView itemDescription;
+	}
+	
+	private static void sortList(ArrayList<GMMapHeader> aItems){
+		Collections.sort(aItems, new NameComparator());
+	}
+	
+	static class NameComparator implements Comparator<Object> {
+		 public int compare(Object obj1, Object obj2)
+	      {
+	            GMMapHeader o1 = (GMMapHeader) obj1;
+	            GMMapHeader o2 = (GMMapHeader) obj2;
+	            return o1.getName().compareToIgnoreCase(o2.getName());
+	      }
+	}
+}
